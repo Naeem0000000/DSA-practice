910ee4702f29cc4112fa410244c8397af87f6937
Naeem0000000 <168430702+Naeem0000000@users.noreply.github.com>
2024-07-06 15:20:39 +0500

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,35 @@
+public class BubbleSort {
+
+    public static void sort(int[] arr) {
+      int n = arr.length;
+      for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+          if (arr[j] > arr[j + 1]) {
+            // Swap elements if out of order
+            int temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
+          }
+        }
+      }
+    }
+  
+    public static void main(String[] args) {
+      int[] arr = {64, 34, 25, 12, 22, 11, 90};
+      System.out.println("Array before sorting: ");
+      printArray(arr);
+  
+      sort(arr);
+  
+      System.out.println("Array after sorting: ");
+      printArray(arr);
+    }
+  
+    public static void printArray(int[] arr) {
+      for (int i : arr) {
+        System.out.print(i + " ");
+      }
+      System.out.println();
+    }
+  }
+  
